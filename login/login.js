9e4c94e0962b0b8334444c427fcc88415ba569ee
exapisde25 <die321d.21@gmail.com>
2026-01-19 17:48:48 -0300

    update: loginjs ahora funciona con jquery (con fines de evaluacion)

--- login/login.js
+++ login/login.js
@@ -1,26 +1,26 @@
-let loginForm = document.getElementById('login-form');
+$(document).ready(function() {
+    function showAlert(message) {
+        let alertBox = $("#alert-message");
+        alertBox.text(message);
+        alertBox.removeClass('d-none');
 
-function showAlert(message) {
-    let alertBox = document.getElementById("alert-message");
-    alertBox.innerText = message;
-    alertBox.classList.remove('d-none');
-
-    setTimeout(function() {
-        alertBox.classList.add('d-none');
-    }, 3000);
-}
-
-loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-
-    let emailBD = localStorage.getItem('email');
-    let passwordBD = localStorage.getItem('password');
-
-    if(email === emailBD && password === passwordBD) {
-        window.location.href = '../menu/menu.html';
-    } else {
-        showAlert('Correo o contraseña incorrectos. Inténtalo de nuevo.');
+        setTimeout(function() {
+            alertBox.addClass('d-none');
+        }, 3000);
     }
+
+    $('#login-form').on('submit', function(event) {
+        event.preventDefault();
+        let email = $('#email').val();
+        let password = $('#password').val();
+
+        let emailBD = localStorage.getItem('email');
+        let passwordBD = localStorage.getItem('password');
+
+        if(email === emailBD && password === passwordBD) {
+            window.location.href = '../menu/menu.html';
+        } else {
+            showAlert('Correo o contraseña incorrectos. Inténtalo de nuevo.');
+        }
+    });
 });
